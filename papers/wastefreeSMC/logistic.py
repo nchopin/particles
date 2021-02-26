6e0b319148fd8789806893c82e444399661384e9
Nicolas Chopin <nicolas.chopin@ensae.fr>
2021-02-26 13:16:01 +0100

    wastefree: logistic example

--- papers/wastefreeSMC/logistic.py
+++ papers/wastefreeSMC/logistic.py
@@ -37,15 +37,17 @@ T, p = data.shape
 # All of the runs are such that N*K or M*P equal 10^5
 
 if dataset_name == 'sonar':
-    MPs = [(50, 2_000), (200, 500), (500, 200)]
-    NKs = [(10_000, 10), (2_500, 40), (1_000, 100)]
+    N0 = 10**5
+    Ks = [10, 40, 160]
+    Ms = [50, 200, 800]
 elif dataset_name == 'pima':
-    MPs = [(20, 500), (50, 200), (100, 100)]
-    NKs = [(10_000, 1), (2000, 5), (500, 20)]
+    N0 = 10**4
+    Ks = [1, 4, 16]
+    Ms = [25, 100, 400]
 elif dataset_name == 'eeg':
-    N = 10 ** 3
-    Ms = [1, 3, 5, 7, 10, 15, 20]
-    # TODO
+    N0 = 10 ** 4
+    Ks = [1, 4, 16]
+    Ms = [25, 100, 400]
 
 # prior & model
 prior = dists.StructDist({'beta':dists.MvNormal(scale=5.,
@@ -59,12 +61,12 @@ class LogisticRegression(ssps.StaticModel):
 
 # algorithms
 # N and values of M set above according to dataset
-nruns = 20  # TODO
+nruns = 50  # TODO
 results = []
 
 # runs
 print('Dataset: %s' % dataset_name)
-for mp, nk in zip(MPs, NKs):
+for M, K in zip(Ms, Ks):
     for i in range(nruns):
         # need to shuffle the data for IBIS
         random.shuffle(data)
@@ -72,19 +74,19 @@ for mp, nk in zip(MPs, NKs):
         for alg_type in ['tempering', 'ibis']:
             for waste in [True, False]:
                 if waste:
-                    M, P = mp
+                    P = N0 // M
+                    N, nsteps = M, P - 1
                     res = {'M': M, 'P': P}
-                    N = M
-                    nsteps = P -1
                 else:
-                    N, K = nk
+                    N = N0 // K
+                    nsteps = K
                     res = {'N': N, 'K': K}
                 if alg_type == 'ibis':
                     fk = ssps.IBIS(model=model, nsteps=nsteps, wastefree=waste)
                 else:
                     fk = ssps.AdaptiveTempering(model=model, nsteps=nsteps, 
                                                 wastefree=waste)
-                pf = particles.SMC(fk=fk, N=N, collect=[Moments], verbose=True)
+                pf = particles.SMC(fk=fk, N=N, collect=[Moments], verbose=False)
                 print('%s, waste:%i, nsteps=%i, run %i' % (alg_type, waste,
                                                            nsteps, i))
                 pf.run()
@@ -94,26 +96,8 @@ for mp, nk in zip(MPs, NKs):
                             'out': pf.summaries,
                             'waste': waste,
                             'cpu': pf.cpu_time})
-            # TODO M > N
-            # if alg_type=='ibis':
-            #     n_eval = N * (T + M * sum([t for t in range(T) if
-            #                                pf.summaries.rs_flags[t]]))
-            # else:
-            #     n_eval = N * T * (1. + M * (len(pf.summaries.ESSs) - 1))
-            #     res['path_sampling'] = pf.X.shared['path_sampling'][-1]
-            #     res['exponents'] = pf.X.shared['exponents']
-            # res['n_eval'] = n_eval
-            results.append(res)
+                results.append(res)
 
-# save results
-##############
-# import pickle
-# pickle.dump({'results':results, 'Ms': Ms, 'N': N, 'nruns': nruns, 'T': T, 'p':p,
-#              'ESSrmin' : ESSrmin, 'data': data},
-#             open('%s_N%i.pickle' % (dataset, N), 'wb'))
-# Then reload like this
-# dp = pickle.load(file_name)
-# locals().update(dp)
 
 # plots
 #######
@@ -121,54 +105,6 @@ savefigs = True  # do you want to save figures as pdfs
 plt.style.use('ggplot')
 pal = sb.dark_palette('white', n_colors=2)
 
-# Compare standard and path sampling estimates of the log-normalising cst
-# plt.figure()
-# diff_est = [(r['out'].logLts[-1] - r['path_sampling'])
-#             for r in results if r['type']=='waste-temp']
-# sb.distplot(diff_est)
-
-# # Figure 17.1: typical behaviour of IBIS
-# typ_ibis = [r for r in results if r['type']=='ibis' and r['M'] == typM][0]
-# typ_ess = typ_ibis['out'].ESSs
-# typ_rs_times = np.nonzero(typ_ibis['out'].rs_flags)[0]
-
-# # Left panel: evolution of ESS
-# fig, ax = plt.subplots()
-# ax.plot(typ_ess, 'k')
-# ax.set(xlabel=r'$t$', ylabel='ESS')
-# if savefigs:
-#     plt.savefig(dataset_name + '_typical_ibis_ess.pdf')
-
-# # Right panel: evolution of resampling times
-# fig, ax = plt.subplots()
-# ax.plot(typ_rs_times[:-1], np.diff(typ_rs_times), 'ko-')
-# ax.set(xlabel=r'$t$', ylabel='duration between successive rs')
-# if savefigs:
-#     plt.savefig(dataset_name + '_typical_ibis_rs_times.pdf')
-
-# # Figure 17.2: evolution of temperature in a typical tempering run
-# typ_temp = [r for r in results if r['type']=='tempering' and r['M'] == typM][0]
-# expnts = typ_temp['exponents']
-# plt.figure()
-# plt.plot(expnts, 'k')
-# plt.xlabel(r'$t$')
-# plt.ylabel('tempering exponent')
-# if savefigs:
-#     plt.savefig(dataset_name + '_typical_tempering_temperatures.pdf')
-
-# # nr evals vs M for both algorithms
-# plt.figure()
-# sb.boxplot(x=[r['M'] for r in results],
-#            y=[r['n_eval'] for r in results],
-#            hue=[r['type'] for r in results],
-#            palette=pal)
-# plt.xlabel('number MCMC steps')
-# plt.ylabel('number likelihood evaluations')
-# if savefigs:
-#     plt.savefig(dataset_name + '_boxplots_nevals_vs_M.pdf')
-
-# Figure 17.3: Box-plots estimate versus number of MCMC steps
-# Left panel: marginal likelihood
 
 titles = ['standard SMC', 'waste-free SMC']
 plots = {'log marginal likelihood': lambda rout: rout.logLts[-1],
