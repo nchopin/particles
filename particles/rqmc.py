49104a1006f23c5f850c14822838197a51587293
adriencorenflos <adrien.corenflos@gmail.com>
2021-11-03 15:25:06 +0200

    Avoid rebuilding the engine for each sampling.

--- particles/rqmc.py
+++ particles/rqmc.py
@@ -1,7 +1,6 @@
 """Randomised quasi-Monte Carlo sequences.
 
 """
-
 TOL = 1e-10
 
 # temporary: remove when scipy 1.7 is available in conda
@@ -10,17 +9,23 @@ try:
 except ImportError:
     qmc = None
 
-def safe_generate(N, d, engine_cls):
-    eng = engine_cls(d)
+
+def _safe_generate(N, eng):
     u = eng.random(N)
     v = 0.5 + (1. - TOL) * (u - 0.5)
     return v
 
-def sobol(N, d):
-    return safe_generate(N, d, qmc.Sobol)
 
-def halton(N, d):
-    return safe_generate(N, d, qmc.Halton)
+def _get_qmc_sampler(engine_cls):
+    instances = dict()
 
-def latin(N, d):
-    return safe_generate(N, d, qmc.LatinHybercube)
+    def sampler(N, d):
+        eng = instances.setdefault(d, engine_cls(d))
+        return _safe_generate(N, eng)
+
+    return sampler
+
+
+sobol = _get_qmc_sampler(qmc.Sobol)
+halton = _get_qmc_sampler(qmc.Halton)
+latin = _get_qmc_sampler(qmc.LatinHypercube)
