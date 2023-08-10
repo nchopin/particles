e21b3dd6cf1d4667dc6071380627b78e9c79a3f4
Nicolas Chopin <nicolas.chopin@ensae.fr>
2023-08-10 16:41:03 +0200

    Revert "Avoid rebuilding the engine for each sampling."
    This reverts commit 49104a1006f23c5f850c14822838197a51587293.
    
    Generating the engine only once seems like a good idea, but there is
    maximum number of samples that may be generated from a given engine.
    Several book scripts fails because of this.

--- particles/rqmc.py
+++ particles/rqmc.py
@@ -5,23 +5,17 @@ from scipy.stats import qmc
 
 TOL = 1e-10
 
-
-def _safe_generate(N, eng):
+def safe_generate(N, d, engine_cls):
+    eng = engine_cls(d)
     u = eng.random(N)
     v = 0.5 + (1.0 - TOL) * (u - 0.5)
     return v
 
+def sobol(N, d):
+    return safe_generate(N, d, qmc.Sobol)
 
-def _get_qmc_sampler(engine_cls):
-    instances = dict()
+def halton(N, d):
+    return safe_generate(N, d, qmc.Halton)
 
-    def sampler(N, d):
-        eng = instances.setdefault(d, engine_cls(d))
-        return _safe_generate(N, eng)
-
-    return sampler
-
-
-sobol = _get_qmc_sampler(qmc.Sobol)
-halton = _get_qmc_sampler(qmc.Halton)
-latin = _get_qmc_sampler(qmc.LatinHypercube)
+def latin(N, d):
+    return safe_generate(N, d, qmc.LatinHybercube)
